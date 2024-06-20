dfa3ebfc66779001ba3d1093d7a92563a985b447
Wil220 <wilker.silva0714@gmail.com>
2024-06-20 19:02:47 -0300

    controller

--- src/projeto_final_bloco_01/Carro.java
+++ src/projeto_final_bloco_01/Carro.java
@@ -2,28 +2,12 @@ package projeto_final_bloco_01;
 
 public class Carro extends Produto {
 
-	public Carro(String nome, String marca, float valor, int ano) {
-		super(nome, marca, valor, ano);
-	}
+    public Carro(String nome, String marca, float valor, int ano) {
+        super(nome, marca, valor, ano);
+    }
 
-	public void visualizar() {
-
-		
-	}
-
-	public String getNome() {
-
-		return null;
-	}
-
-	public void setValor(float novoValor) {
-
-		
-	}
-
-	public void setAno(int novoAno) {
-
-		
-	}
-
+    @Override
+    public void visualizar() {
+        super.visualizar();
+    }
 }
--- src/projeto_final_bloco_01/Menu.java
+++ src/projeto_final_bloco_01/Menu.java
@@ -1,8 +1,5 @@
 package projeto_final_bloco_01;
 
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Menu {
@@ -10,72 +7,93 @@ public class Menu {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        List<Carro> carros = new ArrayList<>();
-
-        Carro carro1 = new Carro("Polo", "Volkswagen", 30000.0f, 2001);
-        carros.add(carro1);
-
-        Carro carro2 = new Carro("911", "Porsche", 1000000.0f, 2024);
-        carros.add(carro2);
-
-        Carro carro3 = new Carro("Uno", "Fiat", 10000.0f, 2015);
-        carros.add(carro3);
-
+        Controller controller = new Controller();
 
         int opcao;
 
-		while (true) {
+        while (true) {
 
-			System.out.println("""
-					             ============================
-					                      ZEN CARROS 
-					             ============================
-					             
-					             1 - PROCURAR CARRO
-					             2 - LISTA DE CARROS
-					             3 - EXCLUIR CARRO
-					             4 - ATAULIAZAR ESTOQUE
-					             5 - SAIR
-					             
-					             ============================
-					""");
+            System.out.println("""
+                                 ============================
+                                          ZEN CARROS 
+                                 ============================
+                                 
+                                 1 - CADASTRAR CARRO
+                                 2 - LISTA DE CARROS
+                                 3 - EXCLUIR CARRO
+                                 4 - ATUALIZAR ESTOQUE
+                                 5 - SAIR
+                                 
+                                 ============================
+                    """);
 
-			opcao = sc.nextInt();
+            System.out.print("Digite a opção desejada: ");
+            opcao = sc.nextInt();
+            sc.nextLine();
 
-			if (opcao == 5) {
-				System.out.println("\nA Zen Carros Agradece a Prefencia!");
-				sobre();
-				sc.close();
-				System.exit(0);
-			}
+            if (opcao == 5) {
+                System.out.println("\nA Zen Carros Agradece a Preferência!");
+                sobre();
+                sc.close();
+                System.exit(0);
+            }
 
-			switch (opcao) {
-			case 1:
-				System.out.println("Escreva o nome a marca e ano do carro que deseja:");
-				break;
-				
-			case 2:
-				System.out.println("Carros Em Nosso Estoque");
-				break;
-				
-			case 3:
-				System.out.println("Excluir Carro ");
-				break;
-				
-			case 4:
-				System.out.println("Aualizar");
-				break;
-				
-			case 5:
-				System.out.println("SAIR");
-			} 
-		}
-	}
-    public void visualizar() {
-    	
+            switch (opcao) {
+                case 1:
+                    System.out.println("Cadastro de Novo Carro");
+                    System.out.print("Digite o nome: ");
+                    String nome = sc.nextLine();
+                    System.out.print("Digite a marca: ");
+                    String marca = sc.nextLine();
+                    System.out.print("Digite o valor: ");
+                    float valor = sc.nextFloat();
+                    System.out.print("Digite o ano: ");
+                    int ano = sc.nextInt();
+                    sc.nextLine();
+
+                    Carro novoCarro = new Carro(nome, marca, valor, ano);
+                    controller.cadastrar(novoCarro);
+                    break;
+
+                case 2:
+                    System.out.println("Carros em Nosso Estoque:");
+                    controller.listarProdutos();
+                    break;
+
+                case 3:
+                    System.out.println("Excluir Carro");
+                    System.out.print("Digite o índice do carro a ser excluído: ");
+                    int indice = sc.nextInt();
+                    controller.deletarProduto(indice);
+                    break;
+
+                case 4:
+                    System.out.println("Atualizar Estoque");
+                    System.out.print("Digite o nome do carro a ser atualizado: ");
+                    String nomeAtualizacao = sc.nextLine();
+
+                    System.out.print("Digite a nova marca: ");
+                    String novaMarca = sc.nextLine();
+                    System.out.print("Digite o novo valor: ");
+                    float novoValor = sc.nextFloat();
+                    System.out.print("Digite o novo ano: ");
+                    int novoAno = sc.nextInt();
+                    sc.nextLine(); 
+
+                    Carro carroAtualizado = new Carro(nomeAtualizacao, novaMarca, novoValor, novoAno);
+                    controller.atualizarProduto(carroAtualizado);
+                    break;
+
+                default:
+                    System.out.println("Opção Inválida! Tente novamente.");
+                    break;
+            }
+        }
     }
-	
-	private static void sobre() {
-		
-	}
+
+    private static void sobre() {
+        System.out.println("\n===================");
+        System.out.println("ZEN CARROS");
+        System.out.println("===================");
+    }
 }
